93325a68b8e35b9c4490083a19da36cd42290cad
Vinicius Lima <vinicius.lima@b2ucorp.local>
2018-04-20 17:02:24 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/myprimeiroapp/MainActivity.java
@@ -0,0 +1,29 @@
+package br.usjt.myprimeiroapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/*
+  *Criado por Vinicius Lima RA: 201313379
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public final static String EXTRA_MESSAGE ="br.usjt.meuprimeiroapp.MESSAGE";
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
